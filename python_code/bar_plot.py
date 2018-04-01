40fdbb3961d40e793a28ff769e4771f81c4f8961
Peng Xu <972728825@qq.com>
2018-04-01 16:17:10 +0100

    plot common bar figure

---
+++ python_code/bar_plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def bar_plot(x_data, y_data, error_data, x_label="", y_label="", title=""):
+    _, ax = plt.subplots()
+    # Draw bars, position them in the center of the tick mark on the x-axis
+    ax.bar(x_data, y_data, color = '#539caf', align = 'center')
+    # Draw error bars to show standard deviation, set ls to 'none'
+    # to remove line between points
+    ax.errorbar(x_data, y_data, yerr = error_data, color = '#297083', ls = 'none', lw = 2, capthick = 2)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
